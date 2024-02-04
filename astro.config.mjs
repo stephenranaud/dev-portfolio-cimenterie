69b36cafa2eccf77fabb9f728b6999b94e87bfe6
stephen-devpaps <stephen.saizonou@paps-app.com>
2024-02-04 16:54:57 +0000

    add site config param

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import bookshop from "@bookshop/astro-bookshop";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://saizonou-portfolio-cimenterie.netlify.app/",
   integrations: [bookshop(), react()],
 });
