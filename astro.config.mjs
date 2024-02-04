388dcc6d9c7992f9a4085339b08f719dc9ef9ab1
stephen-devpaps <stephen.saizonou@paps-app.com>
2024-02-04 16:49:58 +0000

    remove import netlify deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import bookshop from "@bookshop/astro-bookshop";
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
