fe0bac110982a4593329e0442f5ec660cd2fadf0
stephen-devpaps <stephen.saizonou@paps-app.com>
2024-02-04 16:47:00 +0000

    config site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import bookshop from "@bookshop/astro-bookshop";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://stephenranaud.github.io",
-  base: "/dev-portfolio-cimenterie",
   integrations: [bookshop(), react()],
 });
